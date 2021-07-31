5f7af86ca0e83cc86ff5577eb52ab13e88303e3e
Iurii Demidov <demidov1109@yandex-team.ru>
2021-07-31 12:46:03 +0300

    Add initial favicons

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,12 @@ export default function Home() {
       <Head>
         <title>Доставка — ваш новый канал продаж</title>
         <meta name="description" content="Подключите ресторан к Яндекс.Еде, самому узнаваемому в России сервису заказа еды с доставкой" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/favicons/manifest.webmanifest"/>
+        <link rel="icon" href="/favicons/favicon.ico" sizes="any"/>
+        <link rel="icon" href="/favicons/icon.svg" type="image/svg+xml"/>
+        <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff"/>
       </Head>
 
       <main className={styles.main}>
